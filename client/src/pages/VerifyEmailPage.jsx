c1eb3cf58e0a9da3918b27177f0820aad4cb104e
Tipu Sultan <tipu@student.iul.ac.in>
2024-11-25 15:22:52 +0530

    fixed  verify page code

--- client/src/pages/VerifyEmailPage.jsx
+++ client/src/pages/VerifyEmailPage.jsx
@@ -25,14 +25,13 @@ const VerifyEmailPage = () => {
 
         const { data } = await axios.get(`${process.env.REACT_APP_API_HOST}/api/users/verify-email?token=${token}`);
         setMessage(data.message);
-      } catch (error) {
-        setMessage(error.response.data.message);
-      } finally {
         setLoading(false);
         setTimeout(() => {
           navigate('/login');
         }, 30000);
-      }
+      } catch (error) {
+        setMessage(error.response.data.message);
+      } 
     };
 
     verifyEmail();
